ddbe42613059587c9eb34703a35a3e51716c8f31
Ethan Sherbondy <sherbondye@gmail.com>
2012-04-07 16:48:45 -0400

    cleaning up bubbles.

--- ui_prototype/bubbles.js
+++ ui_prototype/bubbles.js
@@ -1,5 +1,5 @@
 (function() {
-  var body, checkForCollisions, collides, force, friend_class, h, hideSwyp, instructions, isVisible, is_touch_device, link, node, positionPreview, real_touches, receiveMessage, register_events, resize, showBubblesAt, sourceWindow, vis, w, x, y;
+  var body, checkForCollisions, collides, force, friendClass, h, hideSwyp, instructions, isTouchDevice, isVisible, link, node, positionPreview, realTouches, receiveMessage, registerEvents, resize, setupBubbles, showBubblesAt, sourceWindow, vis, w, x, y;
 
   x = 0;
 
@@ -17,28 +17,11 @@
 
   sourceWindow = void 0;
 
+  body = void 0;
+
+  vis = void 0;
+
   force = d3.layout.force();
-
-  body = d3.select("body");
-
-  vis = body.append("svg:svg").attr("class", "hidden");
-
-  receiveMessage = function(event) {
-    var eType, ex, ey, touches;
-    console.log("RECEIVED: " + JSON.stringify(event.data));
-    sourceWindow = event.source;
-    eType = event.data.e;
-    touches = event.data.touches;
-    ex = touches[0] - 100;
-    ey = touches[1] - 100;
-    console.log(eType);
-    if (eType === "dragstart") {
-      console.log("show bubbles");
-      positionPreview(ex, ey);
-      $("#preview").attr("src", event.data.img);
-      return showBubblesAt(ex, ey);
-    }
-  };
 
   instructions = {
     "default": "Drag the content onto the person you want to send it to.",
@@ -46,30 +29,13 @@
     sending: "Sending now..."
   };
 
-  is_touch_device = "ontouchstart" in document.documentElement;
+  isTouchDevice = "ontouchstart" in document.documentElement;
 
-  collides = function(el, ex, ey) {
-    var rect;
-    rect = el.getBoundingClientRect();
-    if ((ex >= rect.left && ex <= rect.right) && (ey >= rect.top && ey <= rect.bottom)) {
-      return true;
-    }
-    return false;
-  };
-
-  real_touches = function(elem) {
-    if (is_touch_device) {
+  realTouches = function(elem) {
+    if (isTouchDevice) {
       return d3.touches(elem)[0];
     } else {
       return d3.mouse(elem);
-    }
-  };
-
-  friend_class = function(d) {
-    if (d.friend) {
-      return "friend";
-    } else {
-      return "stranger";
     }
   };
 
@@ -81,6 +47,23 @@
     return force.start();
   };
 
+  collides = function(el, ex, ey) {
+    var rect;
+    rect = el.getBoundingClientRect();
+    if ((ex >= rect.left && ex <= rect.right) && (ey >= rect.top && ey <= rect.bottom)) {
+      return true;
+    }
+    return false;
+  };
+
+  friendClass = function(d) {
+    if (d.friend) {
+      return "friend";
+    } else {
+      return "stranger";
+    }
+  };
+
   checkForCollisions = function(ex, ey) {
     var collisionCount;
     collisionCount = 0;
@@ -88,13 +71,13 @@
       var collision;
       collision = collides(this, ex, ey);
       if (collision) collisionCount += 1;
-      return d3.select(this).attr("class", (collision ? "hovered" : friend_class(d)));
+      return d3.select(this).attr("class", (collision ? "hovered" : friendClass(d)));
     });
     return $("#instructions").text(instructions[(collisionCount > 0 ? "drop" : "default")]);
   };
 
   hideSwyp = function() {
-    node.attr("class", friend_class);
+    node.attr("class", friendClass);
     vis.attr("class", "hidden");
     $("#preview").hide();
     isVisible = false;
@@ -115,22 +98,22 @@
     });
   };
 
-  register_events = function() {
-    var events, mouse_events, touch_events;
-    touch_events = ["touchstart", "touchmove", "touchend"];
-    mouse_events = ["mousedown", "mousemove", "mouseup"];
-    events = (is_touch_device ? touch_events : mouse_events);
+  registerEvents = function() {
+    var events, mouseEvents, touchEvents;
+    touchEvents = ["touchstart", "touchmove", "touchend"];
+    mouseEvents = ["mousedown", "mousemove", "mouseup"];
+    events = (isTouchDevice ? touchEvents : mouseEvents);
     return body.on(events[0], function() {
       var bod, xy;
       bod = this;
       d3.event.preventDefault();
       d3.event.stopPropagation();
-      xy = real_touches(bod);
+      xy = realTouches(bod);
       return showBubblesAt(xy[0], xy[1]);
     }).on(events[1], function(e) {
       var xy;
       if (isVisible) {
-        xy = real_touches(this);
+        xy = realTouches(this);
         $("#preview").show();
         positionPreview(xy[0], xy[1]);
         return checkForCollisions(xy[0], xy[1]);
@@ -140,32 +123,29 @@
     });
   };
 
-  d3.json("graph.json", function(json) {
-    force.nodes(json.nodes).links(json.links).gravity(0).distance(100).charge(-500).start();
-    link = vis.selectAll("line.link").data(json.links).enter().append("svg:line").attr("class", "link").attr("x1", function(d) {
-      return d.source.x;
-    }).attr("y1", function(d) {
-      return d.source.y;
-    }).attr("x2", function(d) {
-      return d.target.x;
-    }).attr("y2", function(d) {
-      return d.target.y;
-    });
-    node = vis.selectAll("g.node").data(json.nodes).enter().append("svg:g").attr("class", function(d) {
-      return friend_class;
-    });
-    node.filter(function(d, i) {
-      return i !== 0;
-    }).append("svg:rect").attr("class", "rect").attr("x", "-16px").attr("y", "-20px").attr("width", "200px").attr("height", "40px");
-    node.append("svg:image").attr("class", "circle").attr("xlink:href", function(d) {
-      return d.picture;
-    }).attr("x", "-16px").attr("y", "-20px").attr("width", "40px").attr("height", "40px");
-    node.append("svg:text").attr("class", "nodetext").attr("dx", 32).attr("dy", ".35em").text(function(d) {
-      return d.name;
-    });
-    return force.on("tick", function(e) {
-      resize();
-      link.attr("x1", function(d) {
+  receiveMessage = function(event) {
+    var eType, ex, ey, touches;
+    console.log("RECEIVED: " + JSON.stringify(event.data));
+    sourceWindow = event.source;
+    eType = event.data.e;
+    touches = event.data.touches;
+    ex = touches[0] - 100;
+    ey = touches[1] - 100;
+    console.log(eType);
+    if (eType === "dragstart") {
+      console.log("show bubbles");
+      positionPreview(ex, ey);
+      $("#preview").attr("src", event.data.img);
+      return showBubblesAt(ex, ey);
+    }
+  };
+
+  setupBubbles = function() {
+    body = d3.select("body");
+    vis = body.append("svg:svg").attr("class", "hidden");
+    return d3.json("graph.json", function(json) {
+      force.nodes(json.nodes).links(json.links).gravity(0).distance(100).charge(-1000).start();
+      link = vis.selectAll("line.link").data(json.links).enter().append("svg:line").attr("class", "link").attr("x1", function(d) {
         return d.source.x;
       }).attr("y1", function(d) {
         return d.source.y;
@@ -174,22 +154,47 @@
       }).attr("y2", function(d) {
         return d.target.y;
       });
-      return node.attr("transform", function(d) {
-        var damper;
-        if (d.index === 0) {
-          damper = 0.1;
-          d.x = x ? d.x + (x - d.x) * (damper + 0.71) * e.alpha : 400;
-          d.y = y ? d.y + (y - d.y) * (damper + 0.71) * e.alpha : 400;
-        }
-        return "translate(" + d.x + "," + d.y + ")";
+      node = vis.selectAll("g.node").data(json.nodes).enter().append("svg:g").attr("class", function(d) {
+        return friendClass;
+      });
+      node.filter(function(d, i) {
+        return i !== 0;
+      }).append("svg:rect").attr("class", "rect").attr("x", "-16px").attr("y", "-20px").attr("width", "200px").attr("height", "40px");
+      node.append("svg:image").attr("class", "circle").attr("xlink:href", function(d) {
+        return d.picture;
+      }).attr("x", "-16px").attr("y", "-20px").attr("width", "40px").attr("height", "40px");
+      node.append("svg:text").attr("class", "nodetext").attr("dx", 32).attr("dy", ".35em").text(function(d) {
+        return d.name;
+      });
+      return force.on("tick", function(e) {
+        resize();
+        link.attr("x1", function(d) {
+          return d.source.x;
+        }).attr("y1", function(d) {
+          return d.source.y;
+        }).attr("x2", function(d) {
+          return d.target.x;
+        }).attr("y2", function(d) {
+          return d.target.y;
+        });
+        return node.attr("transform", function(d) {
+          var damper;
+          if (d.index === 0) {
+            damper = 0.1;
+            d.x = x ? d.x + (x - d.x) * (damper + 0.71) * e.alpha : 400;
+            d.y = y ? d.y + (y - d.y) * (damper + 0.71) * e.alpha : 400;
+          }
+          return "translate(" + d.x + "," + d.y + ")";
+        });
       });
     });
-  });
+  };
 
   $(function() {
     window.addEventListener("message", receiveMessage, false);
-    $("#instructions").text(instructions["default"]);
-    return register_events();
+    setupBubbles();
+    registerEvents();
+    return $("#instructions").text(instructions["default"]);
   });
 
 }).call(this);
